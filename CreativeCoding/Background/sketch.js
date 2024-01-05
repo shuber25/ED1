a8597d34ea638cadcf74de41b75175e5d6da9833
shuber25 <144743058+shuber25@users.noreply.github.com>
2024-01-05 11:00:58 -0500

    Background

---
+++ CreativeCoding/Background/sketch.js
@@ -0,0 +1,8 @@
+function setup() { //runs one time
+createCanvas(windowWidth, windowHeight); //canvas size to window
+background (250,210,210); //red background 
+}
+
+function draw() { //runs in a loop
+
+}
