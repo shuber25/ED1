8d0699d3bb62e6fcb3205d3bc2a74ab90ffe1f9b
shuber25 <144743058+shuber25@users.noreply.github.com>
2024-01-26 12:31:02 -0500

    update

--- CreativeCoding/FINALBackground/sketch.js
+++ CreativeCoding/FINALBackground/sketch.js
@@ -1,25 +1,35 @@
+//adding a GIF to my Grid
+
+let gif;
+
+	function preload () {
+	gif = loadImage ('assets/GIF_Huber.gif');
+	}
+
 function setup() {
   //runs one time
-  createCanvas(windowWidth,windowHeight);
-    background('lightpink');
-  fill ('lightblue');
+  createCanvas(windowWidth, windowHeight);
+  fill('lightgrey');
+  stroke ('white');
+  strokeweight (1);
 }
 
 function draw() {
   //runs in a loop
   
-var num = 50;
-  var sideLen = windowWidth/num;
+  background ('light blue');
   
+var num = 8
+  var sideLen = windowWidth
+  
+  var sidelen = windowWidth/num;
   for(var y = 0; y < windowHeight; y = y + sideLen){ //Loop to create rows in the y direction
      for(var x = 0; x < windowWidth; x = x + sideLen) { //loop to create a row of squares in the x direction
-    
-   quad (x, y, x + sideLen, y, x + sideLen, y + sideLen, x, y + sideLen);
-    
+    image (gif, x, y, sideLen, sideLen);
     }
   }
 }
 
-function windowResized (){
-  resizeCanvas (windowWidth, windowHeight)
+function windowResizd (){
+  resizeCanvas (windowWidth, windowHeight);
 }
