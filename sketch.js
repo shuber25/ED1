b6b446e91e9b6550e45141029642be324e2ea077
shuber25 <144743058+shuber25@users.noreply.github.com>
2024-01-30 10:53:52 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,36 +1,35 @@
-//adding a GIF to my Grid
+//adding a GIF to my grid
 
 let gif;
 
-	function preload () {
-	gif = loadImage ('Assets/GIF_Huber.gif' );
-	}
+function preload (){
+gif = loadImage ('Assets/GIF_Huber.gif');
+}
 
 function setup() {
   //runs one time
-  createCanvas(windowWidth,windowHeight);
-  fill('white');
-  strokeweight(1);
-  
+  createCanvas(windowWidth, windowHeight);
+  fill('magenta');
+  strokeWeight(1);
 }
 
 function draw() {
   //runs in a loop
-  
-  background ('light blue');
-  
-var num = 8;
-   var sideLen = windowWidth
+
+  background ('red');
+
+  var num = 50;
   var sideLen = windowWidth/num;
-  
-  
-  for(var y = 0; y < 2 * windowHeight; y = y + sideLen){ //Loop to create rows in the y direction
-     for(var x = 0; x < 2* windowWidth; x = x + sideLen) { //loop to create a row of squares in the x direction
-    image (gif, x, y, sideLen, sideLen);
+
+  for (var y = 0; y < 2 * windowHeight; y = y + sideLen) {
+    //loop to create a grid in the y direction
+
+    for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
+image (gif, x, y, sideLen, sideLen);
     }
   }
 }
 
-function windowResized (){
+function windowResized(){
   resizeCanvas (windowWidth, windowHeight);
 }
